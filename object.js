52b6c740bb40364c33c4d9f022558448545525a3
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-07-11 08:46:06 +0600

    Auto-update on Sat, Jul 11, 2026  8:46:06 AM

---
+++ object.js
@@ -0,0 +1,18 @@
+const myObj = {
+  name: 'towfiq',
+  role: 'developer',
+  self:{
+    fName: 'rakib',
+    mName: 'rohima'
+  }
+}
+
+Object.freeze(myObj.name)
+myObj.name = 'Saiful'
+console.log(myObj.name);
+
+//myObj.roll = '234'
+//console.log(myObj["role"]);
+// delete myObj.name
+// console.log(myObj);
+
