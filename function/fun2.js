76a759ef9741b66390e65a1e05323434e2757ca6
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-07-11 22:36:38 +0600

    Auto-update on Sat, Jul 11, 2026 10:36:38 PM

---
+++ function/fun2.js
@@ -0,0 +1,7 @@
+function myFunc(num1, num2){
+  const result = num1 + num2
+  console.log(result);
+  
+}
+myFunc(5,5)
+myFunc(5,15)
---
+++ function/fun3.js
@@ -0,0 +1,13 @@
+// function myFun(num1, num2){
+//   const res = num1 + num2
+//   return res
+// }
+// const fRes = myFun(5, 7)
+// console.log(fRes);
+
+function func(num1, num2, num3) {
+  const a = num1 + num2 + num3
+  return a
+}
+const res= func(1,2,3)
+console.log(res);
