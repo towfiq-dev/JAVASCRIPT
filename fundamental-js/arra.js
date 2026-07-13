d2f27ad8d2bcfda9dd07a112b76aab71c387b23a
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-07-13 07:22:27 +0600

    Auto-update on Mon, Jul 13, 2026  7:22:27 AM

--- fundamental-js/arra.js
+++ fundamental-js/arra.js
@@ -1,4 +1,35 @@
-const myArray = ['a', 's', 'f', 'g', 'w', 'r']
+// // const myArray = ['a', 's', 'f', 'g', 'w', 'r']
 
-const res=  myArray.unshift('A')
-console.log(res);
+// // const res = Array.isArray(myArray)
+// // console.log(res);
+
+// // //const res=  myArray.unshift('A')
+// // const res = myArray.length
+// // const res1 = myArray.indexOf('s')
+// // console.log(res1);
+// // myArray[1] = "asdf"
+// // console.log(myArray);
+// // if (6 === myArray) {
+// //   console.log('acchi');
+  
+// // }else{
+// //   console.log('nai');
+  
+// // }
+
+// const arrr1 = [1,2,3,4,5]
+// const arrr2 = [6,7,8,9,10]
+// const res = arrr1.concat(arrr2)
+// //console.log(res);
+
+// const arrr3 = [1,2,3,4,5]
+// const ress = arrr3.slice(1,3)
+// console.log(ress);
+
+const nums = ['s', 'f', 'g', 'e', 't', 'u']
+
+const res1 = nums.slice(1, 2)
+const res2 = nums.splice(1, 2, 3,5)
+console.log(res1);
+console.log(res2);
+console.log(nums);
---
+++ fundamental-js/loop.js
@@ -0,0 +1,21 @@
+// const friends = ['q', 'e', 'g', 'r', 'y', 'u', 'h', 'k', 'p']
+
+// for(i= 0; i < friends.length; i++){
+//   //console.log(friends[i]);
+// }
+// let i = 0;
+// while(i<friends.length){
+// //console.log(friends[i]);
+// i++
+// }
+
+const nums = [4,3,6,2,8,5,7,1,9,12,10,11]
+const sorted = nums.sort((a,b)=>{return a-b})
+const sorted1 = nums.sort((a,b)=>{return b-a})
+console.log(sorted);
+console.log(sorted1);
+
+// for(i=nums.length-1; i>=0; i--){
+//   console.log(nums[i]);
+  
+// }
